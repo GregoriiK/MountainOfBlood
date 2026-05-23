4d303aecd0565ccb2aa1d88dc0fa080e78da2520
Patryk Kowalczyk <108098577+GregoriiK@users.noreply.github.com>
2026-05-23 10:40:10 +0200

    MainMenu update

--- Assets/MainMenuManager.cs
+++ Assets/MainMenuManager.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 public class MainMenuManager : MonoBehaviour
 {
     Scene scene;
-
+    [SerializeField] GameObject optionsPanel;
+    [SerializeField] Slider mainVolumeSlider;
+    [SerializeField] TextMeshProUGUI mainVolumeText;
     private void Start()
     {
         scene = SceneManager.GetActiveScene();
+        ToggleOptionsPanel();
     }
 
     public void StartNewGame()
     {
         SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ToggleOptionsPanel()
+    {
+        if (optionsPanel.activeInHierarchy == true)
+        {
+            optionsPanel.SetActive(false);
+        }
+        else
+        {
+            optionsPanel.SetActive(true);
+        }
     }
 }
